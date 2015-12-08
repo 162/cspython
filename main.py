9b7c4fcabbc1634366756190edcad966849b9850
justgecko <kudinov.anton96@gmail.com>
2015-12-08 19:43:38 +0300

    upd: txt results

--- main.py
+++ main.py
@@ -28,11 +28,21 @@ def main(sems):
     for i in range(sems):
         task_ids.append(parse_file('tasks'+str(i+1)+'.txt', True))
     students = [Student(i, len(task_ids), [len(j) for j in task_ids]) for i in names]
-    for n_sem in range(len(task_ids)):
-        for n_task in range(len(task_ids[n_sem])):
-            print "TASK:", task_ids[n_sem][n_task]
-            scan_task(task_ids[n_sem][n_task], n_sem, n_task, students)
-
+    try:
+        for n_sem in range(len(task_ids)):
+            for n_task in range(len(task_ids[n_sem])):
+                print "TASK:", task_ids[n_sem][n_task]
+                scan_task(task_ids[n_sem][n_task], n_sem, n_task, students)
+    except IndexError():
+        pass
+    for i in range(sems):
+        txt = 'name\t' + '\t'.join([str(x) for x in task_ids[i]])
+        for student in students:
+            l1, l2 = student.get_sem_stats(i)
+            txt += l1+'\t'+l2
+        f = open('students/'+str(i)+'.txt', 'w')
+        f.write(txt)
+        f.close()
 
 
 main(6)
